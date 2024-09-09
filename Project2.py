abc787d55ed9f9339f8d094fd6a7c50a6ff818b8
adas22156@gmail.com <ankita.job22156@gmail.com>
2024-09-10 02:13:22 +0530

    Rent calculator

--- Project2.py
+++ Project2.py
@@ -1,10 +1,11 @@
-person= input("Enter the no of person live in room? =>")
-room_rent= input("Per Month Room Rent? =>")
-E_unit= input("Spend Unit in one month? =>")
-E_Unit_Cost= input("Per unit electric cost? =>")
-food= input("Odered food total charge? =>")
+person= int(input("Enter the no of person live in room? =>"))
+room_rent= int(input("Per Month Room Rent? =>"))
+E_unit= int(input("Spend Unit in one month? =>"))
+E_Unit_Cost= int(input("Per unit electric cost? =>"))
+food= int(input("Odered food total charge? =>"))
 
-electric= E_unit*E_Unit_Cost
+electric= int(E_unit * E_Unit_Cost)
 pay_person= (room_rent+ electric+ food)//person
 
+print(">>>>>>>>>>*<<<<<<<<<<")
 print("Each person have to pay :",pay_person)
