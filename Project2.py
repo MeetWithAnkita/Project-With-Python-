904dc25e3a0579bef9e5d8b2ed00cc0f397c005b
adas22156@gmail.com <ankita.job22156@gmail.com>
2024-09-10 02:07:41 +0530

    Rent Calculation

--- Project2.py
+++ Project2.py
@@ -1,7 +1,7 @@
 person= input("Enter the no of person live in room? =>")
 room_rent= input("Per Month Room Rent? =>")
 E_unit= input("Spend Unit in one month? =>")
-E_Unit_Cost= input("Per unit electric cost? =>)
+E_Unit_Cost= input("Per unit electric cost? =>")
 food= input("Odered food total charge? =>")
 
 electric= E_unit*E_Unit_Cost
